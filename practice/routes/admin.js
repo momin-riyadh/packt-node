1f7107b43848d14530175eea73f24662acd3d4eb
momin-riyadh <mominriyadh@gmail.com>
2023-11-18 15:53:34 +0600

    fix and deploy

--- practice/routes/admin.js
+++ practice/routes/admin.js
@@ -1,49 +1,15 @@
 const path = require('path');
-const express = require('express');
-const rootDir = require('../util/path');
-const multer = require('multer');
 
+const express = require('express');
+
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
-
-
-
-// Multer configuration
-const storage = multer.diskStorage({
-  destination: './public/images/products',
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({ storage: storage });
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
-// Update your add-product route
-router.post('/add-product', upload.single('image'), (req, res, next) => {
-  console.log(req.file);
-  const newProduct = {
-    title: req.body.title,
-    price: req.body.price,
-    description: req.body.description,
-    imageUrl: req.file ? '/images/products/' + req.file.filename : '/images/default.svg',
-  };
+router.post('/add-product', productsController.postAddProduct);
 
-  products.push(newProduct);
-  res.redirect('/admin/add-product');
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
