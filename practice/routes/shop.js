8dbb473100831b4b0e54177d9c06e20f468d0684
momin-riyadh <mominriyadh@gmail.com>
2023-11-19 06:51:58 +0600

    fix and deploy

--- practice/routes/shop.js
+++ practice/routes/shop.js
@@ -1,9 +1,6 @@
 const path = require('path');
-
 const express = require('express');
-
 const productsController = require('../controllers/products');
-
 const router = express.Router();
 
 router.get('/', productsController.getProducts);
